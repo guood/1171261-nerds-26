ecb8fe97e038b1400d0765b4898d3fc17aa107da
Andrey Sadovodov <kdos@bk.ru>
2019-10-23 00:09:39 +0300

    оформление

---
+++ js/script-min.js
@@ -0,0 +1,2 @@
+"use strict";let writeUsButton=document.querySelector(".write-us-button");let writeUsModal=document.querySelector(".modal-write-us");let modalCloseButton=writeUsModal.querySelector(".modal-close-button");modalCloseButton.addEventListener("click",function(){writeUsModal.classList.remove("show-modal")})
+writeUsButton.addEventListener("click",function(evt){evt.preventDefault();writeUsModal.classList.add("show-modal")})
